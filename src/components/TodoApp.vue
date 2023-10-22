a2654da233e2bb70e82bff1a39e65ec6e902a88d
hilbert <fhilbert78@gmail.com>
2023-10-22 19:11:42 +0200

    init Todo with pinia

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,23 @@
+<script setup>
+import TodoForm from "./TodoForm.vue";
+import TodoList from "./TodoList.vue";
+</script>
+
+<template>
+	<div>
+		<div class="todo-app">
+			<h1>To Do List</h1>
+			<todo-form />
+			<todo-list />
+		</div>
+	</div>
+</template>
+<style scoped>
+.todo-app {
+	color: black;
+	padding: 5vh;
+	min-height: 100vh;
+	width: 30vw;
+	background-color: #f7f9fc;
+}
+</style>
